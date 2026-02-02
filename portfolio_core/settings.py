21194eeabc4ac6395ecde1c81c6c564a5bde26ea
Henry Wanjiru <haronkuria2015@gmail.com>
2026-02-03 01:31:19 +0300

    added cloudinaf

--- portfolio_core/settings.py
+++ portfolio_core/settings.py
@@ -132,9 +132,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': "dx9bfuchk",
-    'API_KEY': 825618243136264,
-    'API_SECRET': "jkqkU45N5mZ2xyvNPicyn-cMe0g",
+    'CLOUD_NAME': 'dx9bfuchk',
+    'API_KEY': '825618243136264',
+    'API_SECRET': 'jkqkU45N5mZ2xyvNPicyn-cMe0g',
 }
 
 MEDIA_URL = '/media/'
