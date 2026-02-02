0a4ba06147a4d9b42674be52dd15a7c0dbc1ef24
Henry Wanjiru <haronkuria2015@gmail.com>
2026-02-03 01:53:05 +0300

    added cloudinafinalfixes

--- portfolio_core/settings.py
+++ portfolio_core/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+	'cloudinary',
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'cloudinary',
-    'cloudinary_storage',
+	
     'web',
 	
 ]
