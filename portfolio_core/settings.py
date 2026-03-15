960038013c002f0f0197d35d3393a23d2c9923da
Henry Wanjiru <haronkuria2015@gmail.com>
2026-03-15 09:43:29 +0300

    changed db to sqlite

--- portfolio_core/settings.py
+++ portfolio_core/settings.py
@@ -78,17 +78,22 @@ WSGI_APPLICATION = 'portfolio_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portifolio_ner5',
+#         'USER': 'portifolio_ner5_user',
+#         'PASSWORD': 'Qvi83QpzQOYQvDgXnxsJzD2fYb07HzZT',
+#         'HOST': 'dpg-d60b7i8gjchc7398mksg-a.oregon-postgres.render.com',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portifolio_ner5',
-        'USER': 'portifolio_ner5_user',
-        'PASSWORD': 'Qvi83QpzQOYQvDgXnxsJzD2fYb07HzZT',
-        'HOST': 'dpg-d60b7i8gjchc7398mksg-a.oregon-postgres.render.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
